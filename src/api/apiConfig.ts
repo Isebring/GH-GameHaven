4300d749f74df50bc2aefd46b108ab301f68153b
Hampus Isebring <hampus.isebring@gmail.com>
2024-11-01 18:31:02 +0100

    testing cors

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: `https://corsproxy.io?https://api.igdb.com/v4/`,
+  baseUrl: `https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4`,
   clientID: import.meta.env.VITE_IGDB_CLIENT_ID,
   authorization: import.meta.env.VITE_IGDB_AUTHORIZATION,
 };
