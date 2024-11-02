6933de2a98d23a7eebfd2dd1b61fbeb3b00f6411
Hampus Isebring <hampus.isebring@gmail.com>
2024-11-03 00:08:51 +0100

    test

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,5 +1,5 @@
 const apiConfig = {
-  baseUrl: `https://thingproxy.freeboard.io/fetch/https://api.igdb.com/v4`,
+  baseUrl: `https://proxy.cors.sh/https://api.igdb.com/v4`,
   clientID: import.meta.env.VITE_IGDB_CLIENT_ID,
   authorization: import.meta.env.VITE_IGDB_AUTHORIZATION,
 };
